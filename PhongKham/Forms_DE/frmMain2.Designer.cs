541efe3c7bb3df54d66848228d63cdf9ad5d6dce
DELL1 <lowji32101@gmail.com>
2022-12-23 18:44:11 +0700

    new main form

---
+++ PhongKham/Forms_DE/frmMain2.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace PhongKham.Forms_DE
+{
+    partial class frmMain2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.benhNhanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thuocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toaThuocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timKiemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tìmBệnhNhânToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tìmThuốcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tìmToaThuocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.báoCáoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tùyChọnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.benhNhanToolStripMenuItem,
+            this.thuocToolStripMenuItem,
+            this.toaThuocToolStripMenuItem,
+            this.timKiemToolStripMenuItem,
+            this.báoCáoToolStripMenuItem,
+            this.tùyChọnToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(926, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // benhNhanToolStripMenuItem
+            // 
+            this.benhNhanToolStripMenuItem.Image = global::PhongKham.Properties.Resources.examination;
+            this.benhNhanToolStripMenuItem.Name = "benhNhanToolStripMenuItem";
+            this.benhNhanToolStripMenuItem.Size = new System.Drawing.Size(112, 24);
+            this.benhNhanToolStripMenuItem.Text = "Bệnh nhân";
+            this.benhNhanToolStripMenuItem.Click += new System.EventHandler(this.benhNhanToolStripMenuItem_Click);
+            // 
+            // thuocToolStripMenuItem
+            // 
+            this.thuocToolStripMenuItem.Image = global::PhongKham.Properties.Resources.pill;
+            this.thuocToolStripMenuItem.Name = "thuocToolStripMenuItem";
+            this.thuocToolStripMenuItem.Size = new System.Drawing.Size(83, 24);
+            this.thuocToolStripMenuItem.Text = "Thuốc";
+            this.thuocToolStripMenuItem.Click += new System.EventHandler(this.thuocToolStripMenuItem_Click);
+            // 
+            // toaThuocToolStripMenuItem
+            // 
+            this.toaThuocToolStripMenuItem.Image = global::PhongKham.Properties.Resources.clipboard;
+            this.toaThuocToolStripMenuItem.Name = "toaThuocToolStripMenuItem";
+            this.toaThuocToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.toaThuocToolStripMenuItem.Text = "Toa thuốc";
+            this.toaThuocToolStripMenuItem.Click += new System.EventHandler(this.toaThuocToolStripMenuItem_Click);
+            // 
+            // timKiemToolStripMenuItem
+            // 
+            this.timKiemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tìmBệnhNhânToolStripMenuItem,
+            this.tìmThuốcToolStripMenuItem,
+            this.tìmToaThuocToolStripMenuItem});
+            this.timKiemToolStripMenuItem.Image = global::PhongKham.Properties.Resources.search;
+            this.timKiemToolStripMenuItem.Name = "timKiemToolStripMenuItem";
+            this.timKiemToolStripMenuItem.Size = new System.Drawing.Size(104, 24);
+            this.timKiemToolStripMenuItem.Text = "Tìm kiếm";
+            // 
+            // tìmBệnhNhânToolStripMenuItem
+            // 
+            this.tìmBệnhNhânToolStripMenuItem.Name = "tìmBệnhNhânToolStripMenuItem";
+            this.tìmBệnhNhânToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.tìmBệnhNhânToolStripMenuItem.Text = "Tìm bệnh nhân";
+            this.tìmBệnhNhânToolStripMenuItem.Click += new System.EventHandler(this.tìmBệnhNhânToolStripMenuItem_Click);
+            // 
+            // tìmThuốcToolStripMenuItem
+            // 
+            this.tìmThuốcToolStripMenuItem.Name = "tìmThuốcToolStripMenuItem";
+            this.tìmThuốcToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.tìmThuốcToolStripMenuItem.Text = "Tìm thuốc";
+            this.tìmThuốcToolStripMenuItem.Click += new System.EventHandler(this.tìmThuốcToolStripMenuItem_Click);
+            // 
+            // tìmToaThuocToolStripMenuItem
+            // 
+            this.tìmToaThuocToolStripMenuItem.Name = "tìmToaThuocToolStripMenuItem";
+            this.tìmToaThuocToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.tìmToaThuocToolStripMenuItem.Text = "Tìm toa thuốc";
+            this.tìmToaThuocToolStripMenuItem.Click += new System.EventHandler(this.tìmToaThuocToolStripMenuItem_Click);
+            // 
+            // báoCáoToolStripMenuItem
+            // 
+            this.báoCáoToolStripMenuItem.Image = global::PhongKham.Properties.Resources.presentation;
+            this.báoCáoToolStripMenuItem.Name = "báoCáoToolStripMenuItem";
+            this.báoCáoToolStripMenuItem.Size = new System.Drawing.Size(97, 24);
+            this.báoCáoToolStripMenuItem.Text = "Báo cáo";
+            // 
+            // tùyChọnToolStripMenuItem
+            // 
+            this.tùyChọnToolStripMenuItem.Image = global::PhongKham.Properties.Resources.menu;
+            this.tùyChọnToolStripMenuItem.Name = "tùyChọnToolStripMenuItem";
+            this.tùyChọnToolStripMenuItem.Size = new System.Drawing.Size(102, 24);
+            this.tùyChọnToolStripMenuItem.Text = "Tùy chọn";
+            // 
+            // frmMain2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayoutStore = System.Windows.Forms.ImageLayout.Stretch;
+            this.BackgroundImageStore = global::PhongKham.Properties.Resources.bh;
+            this.ClientSize = new System.Drawing.Size(926, 601);
+            this.Controls.Add(this.menuStrip1);
+            this.IconOptions.Image = global::PhongKham.Properties.Resources._3358576;
+            this.Name = "frmMain2";
+            this.Text = "Phòng khám tư nhân";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem benhNhanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thuocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toaThuocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem timKiemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tìmBệnhNhânToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tìmThuốcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tìmToaThuocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem báoCáoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tùyChọnToolStripMenuItem;
+    }
+}
---
+++ PhongKham/Forms_DE/frmMain2.cs
@@ -0,0 +1,52 @@
+﻿using DevExpress.XtraEditors;
+using PhongKham.Modules;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhongKham.Forms_DE
+{
+    public partial class frmMain2 : DevExpress.XtraEditors.XtraForm
+    {
+        public frmMain2()
+        {
+            InitializeComponent();
+        }
+
+        private void benhNhanToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Utilities.openForm(new Forms_DE.frmBenhNhan());
+        }
+
+        private void toaThuocToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Utilities.openForm(new Forms_DE.frmToaThuoc());
+        }
+
+        private void thuocToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Utilities.openForm(new Forms_DE.frmThuoc());
+        }
+
+        private void tìmBệnhNhânToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Utilities.openForm(new Forms_DE.frmTimBenhNhan());
+        }
+
+        private void tìmThuốcToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Utilities.openForm(new Forms_DE.frmTimThuoc());
+        }
+
+        private void tìmToaThuocToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Utilities.openForm(new Forms_DE.frmTimToaThuoc());
+        }
+    }
+}
